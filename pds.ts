79fab983cacb255313db89debd8846b499d02d77
Keylan Jensen <keylan118@gmail.com>
2024-02-28 20:37:30 -0700

    Change Pod location variable to an entity variable inside Pico

--- pds.ts
+++ pds.ts
@@ -1,69 +1,75 @@
 import { krl, KrlCtx } from "krl-stdlib";
 import { PicoEngineCore } from "../PicoEngineCore";
-import {
+/**import {
   overwriteFile,
   saveFileInContainer,
-  deleteFile
-} from "@inrupt/solid-client";
+  deleteFile,
+} from "@inrupt/solid-client";*/
 
-let pod_URL:string | undefined = undefined;
+const POD_ENT_NAME : string = "podURL";
+//let pod_URL:string | undefined = undefined;
+const MAX_FILE_SIZE : number = 50;
 
-function getPod() : string | undefined {
-	return pod_URL;
-}
-function setPod(new_URL : string) {
-	pod_URL = new_URL;
-}
+const getPod = krl.Function([], async function() : Promise<string | undefined> {
+	return this.rsCtx.getEnt(POD_ENT_NAME);
+});
+const setPod = krl.Action(["new_URL"], function(new_URL : string | undefined) {
+	this.rsCtx.putEnt(POD_ENT_NAME, new_URL);
+});
 
-function isPodConnected() : boolean {
-	if (!pod_URL) {
+const isPodConnected = krl.Function([], async function() : Promise<boolean> {
+	if (!await getPod(this, [])) {
 		return false;
 	}
 	return true;
-}
+});
 
 async function createFile(fileURL : string) : Promise<File> {
-  let response = await fetch(fileURL);
-  console.log("");
-  console.log(response);
-  console.log("");
-  console.log(response.body);
-  let data = response.body;
+  let response = fetch(fileURL);
+  let data = await response.then(resp => resp.blob());
+  //this.log(data);
+  let data_type : string | null = await response.then(resp => resp.headers.get('content-type'));
   let metadata = {
-    type: response.headers.get('Content-Type');
+    type: <string | undefined>data_type,
   };
   let file = new File([data], "test", metadata);
   return file;
 }
 
-const pds: krl.Module = {
-	connect_pod: krl.Action(["podURL"], function(podURL : string) {
-		if (isPodConnected()) {
-			throw "pds:connect_pod cannot connect an already-connected Pico.";
-		}
-		setPod(podURL);
-		return pod_URL;
-	}),
+const connect_pod = krl.Action(["podURL"], async function(podURL : string) {
+	if (await isPodConnected(this, [])) {
+		throw "pds:connect_pod cannot connect an already-connected Pico.";
+	}
+	await setPod(this, [podURL]);
+	return getPod(this, []);
+});
+const disconnect_pod = krl.Action([], async function() {
+	if (!await isPodConnected(this, [])) {
+		throw "pds:disconnect_pod cannot disconnect an unconnected Pico.";
+	}
+	await setPod(this, [undefined]);
+	return "Disconnected successfully.";
+});
+const store = krl.Action(["fileURL"], async function(fileURL : string) {
+	if (!await isPodConnected(this, [])) {
+		throw "pds:store needs a Pod to be connected.";
+	}
+	let podLocation : string = "";
 	
-	disconnect_pod: krl.Action([], function() {
-		if (!isPodConnected()) {
-			throw "pds:disconnect_pod cannot disconnect an unconnected Pico.";
-		}
-		pod_URL = undefined;
-		return "Disconnected successfully.";
-	}),
-	
-	store: krl.Action(["fileURL"], function(fileURL : string) {
-		if (!isPodConnected()) {
-			throw "pds:store needs a Pod to be connected.";
-		}
-		let podLocation : string = "";
-		
-		let file : File = await createFile(fileURL);
+	let file : File = await createFile(fileURL);
 
-		
-		overwriteFile(getPod() + podLocation, file, { contentType: file.type });
-	}),
+	
+	//overwriteFile(getPod() + podLocation, file, { contentType: file.type });
+});
+
+const pds: krl.Module = {
+	connect_pod: connect_pod,
+	disconnect_pod: disconnect_pod,
+	store: store,
+
+	// getPod : getPod, //Private KRL helper function, does not need to be exported
+	// setPod : setPod, //Private KRL helper function, does not need to be exported
+	// isPodConnected : isPodConnected //Private KRL helper function, does not need to be exported
 }
 
 export default pds;
