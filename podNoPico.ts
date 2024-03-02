f304e04bd7858066a4bb053762e6fead62415b1e
KyerHarris <101298443+KyerHarris@users.noreply.github.com>
2024-03-01 18:11:31 -0700

    Update podNoPico.ts
    Demo file

--- podNoPico.ts
+++ podNoPico.ts
@@ -15,12 +15,15 @@ import {
     isRawData,
     getContentType, 
     getSourceUrl,
+    createContainerAt,
+    deleteContainer,
+    deleteFile,
 } from '@inrupt/solid-client';
 import {
     Session,
 } from '@inrupt/solid-client-authn-node'
 
-let url : string = 'http://localhost:3000/test';
+let url : string = 'http://localhost:3000/test/';
 let webID : string = 'http://localhost:3000/test/profile/card#me';
 let myClientID : string = 'testToke_1f5e0802-b16e-44d3-a0e5-78aecbfbbbda';
 let myClientSecret : string = '6cff217d1c31af7a991e6356608af95f89b10c468da51f498314e4107e38ef3f86cca85260ccaed91c9f0e7936b6b47c9281ee45451d86332248b728db8041cc';
@@ -31,30 +34,63 @@ function setUrl(newUrl : string) {
 }
 
 async function login() {
-    session.login({
+    await session.login({
         clientId: myClientID,
         clientSecret: myClientSecret,
         oidcIssuer: 'http://localhost:3000'
     })
     .then(() => {
         console.log('\nLogin successful!\n');
-        readFile();
-        // uploadFile();
     })
     .catch(error => {
         console.error('Error login:', error);
     });
+    // await first();
+    await second();
+}
+
+async function ls() {
+    let data = await fetch(url)
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(data) {
+            return data;
+        });
+
+    data = data.substring(data.indexOf("contains"));
+    data = data.substring(9, data.indexOf("\n"));
+    let directory = data.split(", ");
+
+    for (let i = 0; i < directory.length; i++) {
+        const element = directory[i];
+        let newEl = element.substring(1, element.indexOf(">"));
+        directory[i] = newEl;
+    }
+    console.log(directory);
+    console.log();
+    return directory;
+}
+
+async function createFolder(){
+    await createContainerAt(`${url}folder/`);
+    console.log('Container created successfully!\n');
+}
+
+async function removeFolder() {
+    await deleteContainer(`${url}folder/`);
+    console.log('Container deleted successfully!\n');
 }
 
 async function readFile() {
     getFile(
-        `${url}/README`,
+        `${url}README`,
         { fetch: fetch }
     )
     .then((file) => {
         console.log(`Fetched a ${getContentType(file)} file from ${getSourceUrl(file)}.`);
         console.log(`The file is ${isRawData(file) ? "not " : ""}a dataset.`);
-        console.log(file);
+        console.log(file + '\n');
     })
 }
 
@@ -65,11 +101,29 @@ async function uploadFile(){
         { fetch: fetch }
     )
     .then(() => {
-        console.log("File uploaded successfully!");
+        console.log("File uploaded successfully!\n");
     })
     .catch(error => {
         console.error("Error uploading file:", error);
     });
+}
+
+async function removeFile() {
+    await deleteFile( `${url}myFile.txt`, { fetch: fetch });
+    console.log('File deleted successfully!\n');
+}
+
+async function first() {
+    await ls()
+    await readFile();
+    await createFolder();
+    await uploadFile();
+}
+async function second() {
+    await ls();
+    await removeFolder();
+    await removeFile();
+    await ls();
 }
 
 login();
