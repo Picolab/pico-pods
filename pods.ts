fe93545498bebd337bb5ccce2c96a61192a69ca6
Keylan Jensen <keylan118@gmail.com>
2024-03-11 10:16:38 -0600

    Modify pods.ts and pods_test.krl to integrate storing files from podsNoPico.ts

--- pods.ts
+++ pods.ts
@@ -1,105 +1,254 @@
 import { krl, KrlCtx } from "krl-stdlib";
-import { PicoEngineCore } from "../PicoEngineCore";
-/**import {
-  overwriteFile,
-  saveFileInContainer,
-  deleteFile,
-} from "@inrupt/solid-client";*/
+import {
+	overwriteFile,
+	saveFileInContainer,
+    getSolidDatasetWithAcl,
+    getAgentAccess,
+    getAgentAccessAll,
+    hasResourceAcl,
+    hasFallbackAcl,
+    hasAccessibleAcl,
+    createAcl,
+    createAclFromFallbackAcl,
+    getResourceAcl,
+    setAgentResourceAccess,
+    saveAclFor,
+    getFile,
+    isRawData,
+    getContentType, 
+    getSourceUrl,
+    createContainerAt,
+    deleteContainer,
+    deleteFile,
+} from "@inrupt/solid-client";
+import {
+    createDpopHeader, 
+    generateDpopKeyPair,
+    buildAuthenticatedFetch,
+} from '@inrupt/solid-client-authn-core';
 
-const POD_ENT_NAME : string = "podURL";
+
+
+const STORAGE_ENT_NAME : string = "__pods_storageURL";
 const MODULE_NAME : string = "pods";
-//let pod_URL:string | undefined = undefined;
-const MAX_FILE_SIZE : number = 50;
+const MAX_FILE_SIZE : number = 500;
+const WEB_ID_ENT_NAME : string = "__pods_webID";
+const CLIENT_ID_ENT_NAME : string = "__pods_clientID";
+const CLIENT_SECRET_ENT_NAME : string = "__pods_clientSecret";
+const TOKEN_URL_ENT_NAME : string = "__pods_tokenURL";
+let authFetch : (typeof fetch);
 
-const getPod = krl.Function([], async function() : Promise<string | undefined> {
-	return this.rsCtx.getEnt(POD_ENT_NAME);
+const getStorage = krl.Function([], async function() : Promise<string | undefined> {
+	return this.rsCtx.getEnt(STORAGE_ENT_NAME);
 });
-const setPod = krl.Action(["new_URL"], async function(new_URL : string | undefined) {
-	this.rsCtx.putEnt(POD_ENT_NAME, new_URL);
+const setStorage = krl.Action(["new_URL"], async function(new_URL : string | undefined) {
+	this.rsCtx.putEnt(STORAGE_ENT_NAME, new_URL);
+});
+const getClientID = krl.Function([], async function() : Promise<string | undefined> {
+	return this.rsCtx.getEnt(CLIENT_ID_ENT_NAME);
+});
+const setClientID = krl.Action(["new_ID"], async function(new_ID : string | undefined) {
+	this.rsCtx.putEnt(CLIENT_ID_ENT_NAME, new_ID);
+});
+const getClientSecret = krl.Function([], async function() : Promise<string | undefined> {
+	return this.rsCtx.getEnt(CLIENT_SECRET_ENT_NAME);
+});
+const setClientSecret = krl.Action(["new_Secret"], async function(new_Secret : string | undefined) {
+	this.rsCtx.putEnt(CLIENT_SECRET_ENT_NAME, new_Secret);
+});
+const getWebID = krl.Function([], async function() : Promise<string | undefined> {
+	return this.rsCtx.getEnt(WEB_ID_ENT_NAME);
+});
+const setWebID = krl.Action(["new_ID"], async function(new_ID : string | undefined) {
+	this.rsCtx.putEnt(WEB_ID_ENT_NAME, new_ID);
+});
+const getTokenURL = krl.Function([], async function() : Promise<string | undefined> {
+	return this.rsCtx.getEnt(TOKEN_URL_ENT_NAME);
+});
+const setTokenURL = krl.Action(["new_URL"], async function(new_URL : string | undefined) {
+	this.rsCtx.putEnt(TOKEN_URL_ENT_NAME, new_URL);
 });
 
-const isPodConnected = krl.Function([], async function() : Promise<boolean> {
-	if (!await getPod(this, [])) {
+
+const isStorageConnected = krl.Function([], async function() : Promise<boolean> {
+	if (!await getStorage(this, [])) {
 		return false;
 	}
 	return true;
 });
 
-async function createFile(fileURL : string) : Promise<File> {
-  let response = fetch(fileURL);
-  let data = await response.then(resp => resp.blob());
-  //this.log(data);
-  let data_type : string | null = await response.then(resp => resp.headers.get('content-type'));
-  let metadata = {
-    type: <string | undefined>data_type,
-  };
-  let file = new File([data], "test", metadata);
-  return file;
+async function createFileObject(fileURL : string) : Promise<File> {
+    let response = await fetch(fileURL);
+    let data = await response.blob()
+    
+	//Get file data type
+    let data_type : string | null = await response.headers.get('content-type');
+    let metadata = {
+      type: <string | undefined>data_type,
+    };
+
+    //Get file name
+    let filename : string | undefined = fileURL.split('/').pop()
+
+    let file = new File([data], <string>filename, metadata);
+    return file;
 }
 
-const connect_pod = krl.Action(["podURL"], async function(podURL : string) {
-	if (await isPodConnected(this, [])) {
-		throw MODULE_NAME + ":connect_pod cannot connect an already-connected Pico.";
-	}
-	await setPod(this, [podURL]);
-	return getPod(this, []);
+//Used to check the size of a file without loading it into memory.
+/*async function getFileSize(url: string): Promise<number> {
+    try {
+        if (url.startsWith('http://') || url.startsWith('https://')) {
+            const response = await axios.head(url);
+            const contentLength = response.headers['content-length'];
+    
+            return contentLength ? parseInt(contentLength, 10) : -1;
+        } else {
+            const stats = fs.statSync(url);
+            return stats.size;
+        }
+    } catch (error) {
+        console.error('An error occurred:', error);
+        return -1;
+    }
+}*/
+
+const authenticate = krl.Action([], async function authenticate() {
+    // A key pair is needed for encryption.
+    // This function from `solid-client-authn` generates such a pair for you.
+    const dpopKey = await generateDpopKeyPair();
+
+    // These are the ID and secret generated in the previous step.
+    // Both the ID and the secret need to be form-encoded.
+    const authString = `${encodeURIComponent(await getClientID(this, []))}:${encodeURIComponent(await getClientSecret(this, []))}`;
+    // This URL can be found by looking at the "token_endpoint" field at
+    // http://localhost:3000/.well-known/openid-configuration
+    // if your server is hosted at http://localhost:3000/.
+    const tokenUrl = await getTokenURL(this, []);
+    const response = await fetch(tokenUrl, {
+    method: 'POST',
+    headers: {
+        // The header needs to be in base64 encoding.
+        authorization: `Basic ${Buffer.from(authString).toString('base64')}`,
+        'content-type': 'application/x-www-form-urlencoded',
+        dpop: await createDpopHeader(tokenUrl, 'POST', dpopKey),
+    },
+    body: 'grant_type=client_credentials&scope=webid',
+    });
+
+    // This is the Access token that will be used to do an authenticated request to the server.
+    // The JSON also contains an "expires_in" field in seconds,
+    // which you can use to know when you need request a new Access token.
+    const { access_token: accessToken } = await response.json();
+
+    authFetch = await buildAuthenticatedFetch(accessToken, { dpopKey });
 });
-const disconnect_pod = krl.Action([], async function() {
-	if (!await isPodConnected(this, [])) {
-		throw MODULE_NAME + ":disconnect_pod cannot disconnect an unconnected Pico.";
+
+
+
+const connect_storage = krl.Action(["storageURL", "webID", "clientID", "clientSecret", "tokenURL"], 
+									async function(
+										storageURL : string, 
+										webID : string, 
+										clientID : string, 
+										clientSecret : string, 
+										tokenURL : string) {
+	if (await isStorageConnected(this, [])) {
+		throw MODULE_NAME + ":connect_storage cannot connect an already-connected Pico.";
 	}
-	await setPod(this, [undefined]);
+	await setStorage(this, [storageURL]);
+	await setWebID(this, [webID]);
+	await setClientID(this, [clientID]);
+	await setClientSecret(this, [clientSecret]);
+	await setTokenURL(this, [tokenURL]);
+	return getStorage(this, []);
+});
+const disconnect_storage = krl.Action([], async function() {
+	if (!await isStorageConnected(this, [])) {
+		throw MODULE_NAME + ":disconnect_storage cannot disconnect an unconnected Pico.";
+	}
+	await setStorage(this, [undefined]);
+	await setWebID(this, [undefined]);
+	await setClientID(this, [undefined]);
+	await setClientSecret(this, [undefined]);
+	await setTokenURL(this, [undefined]);
 	return "Disconnected successfully.";
 });
+
 const store = krl.Action(["fileURL"], async function(fileURL : string) {
-	if (!await isPodConnected(this, [])) {
+	if (!await isStorageConnected(this, [])) {
 		throw MODULE_NAME + ":store needs a Pod to be connected.";
 	}
-	let podLocation : string = "";
+	let storageRelativeLocation : string = "/test/container/";
 	
-	let file : File = await createFile(fileURL);
+	//Used to check file size prior to loading a file into memory
+	//Considered unnecessary
+	/*const size = getFileSize(fileURL);
+    if (await size / (1024*1024) > LIMIT) {
+        throw "The file size exceed 500 MB";
+    }*/
+	
+    let file : File = await createFileObject(fileURL);
 
-	
-	//overwriteFile(getPod() + podLocation, file, { contentType: file.type });
+    saveFileInContainer(
+        await getStorage(this, []) + storageRelativeLocation + file.name,
+        file,
+        { fetch: authFetch }
+    )
+    .then(() => {
+        this.log.debug("File uploaded successfully!\n");
+    })
+    .catch(error => {
+        this.log.error("Error uploading file:", error);
+    });
 });
-const ls = krl.Function(["directoryURL"], async function(directoryURL : string) {
-    if (!await isPodConnected(this, [])) {
-		throw MODULE_NAME + ":ls cannot list Pod contents for an unconnected Pico.";
+const overwrite = krl.Action(["fileURL"], async function(fileURL : string) {
+	if (!await isStorageConnected(this, [])) {
+		throw MODULE_NAME + ":store needs a Pod to be connected.";
 	}
+	let storageRelativeLocation : string = "/test/container/";
+	
+	//Used to check file size prior to loading a file into memory
+	//Considered unnecessary
+	/*const size = getFileSize(fileURL);
+    if (await size / (1024*1024) > LIMIT) {
+        throw "The file size exceed 500 MB";
+    }*/
+	
+    let file : File = await createFileObject(fileURL);
 
-    let newURL = await getPod(this, []);
-    newURL = newURL + directoryURL;
-
-    let data = await fetch(newURL)
-        .then(function(response) {
-            return response.text();
-        })
-        .then(function(data) {
-            return data;
-        });
-
-    data = data.substring(data.indexOf("contains"));
-    data = data.substring(9, data.indexOf("."));
-    let directory = data.split(", ");
-
-    for (let i = 0; i < directory.length; i++) {
-        const element = directory[i];
-        let newEl = element.substring(1, element.indexOf(">"));
-        directory[i] = newEl;
-    }
-    //console.log(directory);
-    return directory;
+    overwriteFile(
+        await getStorage(this, []) + storageRelativeLocation + file.name,
+        file,
+        { fetch: authFetch }
+    )
+    .then(() => {
+        this.log.debug("File uploaded successfully!\n");
+    })
+    .catch(error => {
+        this.log.error("Error uploading file:", error);
+    });
 });
+
+
+
+
 
 const pods: krl.Module = {
-	connect_pod: connect_pod,
-	disconnect_pod: disconnect_pod,
+	connect_pod: connect_storage,
+	disconnect_pod: disconnect_storage,
 	store: store,
-    ls: ls,
+	overwrite: overwrite,
 
-	// getPod : getPod, //Private KRL helper function, does not need to be exported
-	// setPod : setPod, //Private KRL helper function, does not need to be exported
+	// getStorage : getStorage, //Private KRL helper function, does not need to be exported
+	// setStorage : setStorage, //Private KRL helper function, does not need to be exported
 	// isPodConnected : isPodConnected //Private KRL helper function, does not need to be exported
+	//The following are helper functions that are exported for testing
+	setClientID: setClientID,
+	setClientSecret: setClientSecret,
+	setWebID: setWebID,
+	setTokenURL : setTokenURL,
+
+	authenticate : authenticate,
 }
 
 export default pods;
--- pods_test.krl
+++ pods_test.krl
@@ -4,18 +4,31 @@ ruleset pods_test {
 	}
 	
 	rule test_connect_pod {
-		select when test connect_pod
-		pods:connect_pod(event:attrs.get("podURL"))
+		select when test connect_storage
+		pods:connect_storage(event:attrs.get("storageURL"),
+						event:attrs.get("webID"),
+						event:attrs.get("clientID"),
+						event:attrs.get("clientSecret"),
+						event:attrs.get("tokenURL"))
 	}
 	
-	rule test_disconnect_pod {
-		select when test disconnect_pod
-		pods:disconnect_pod()
+	rule test_disconnect_storage {
+		select when test disconnect_storage
+		pods:disconnect_storage()
+	}
+	
+	rule test_authenticate_storage {
+		select when test authenticate_storage
+		pods:authenticate()
 	}
 	
 	rule test_store_file {
 		select when test store_file
 		pods:store(event:attrs.get("fileURL"))
+	}
+	rule test_overwrite_file {
+		select when test overwrite_file
+		pods:overwrite(event:attrs.get("fileURL"))
 	}
 
 	rule test_ls {
