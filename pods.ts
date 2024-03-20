0069aeac67d2652590bcc734fb3ade41bb999080
Keylan Jensen <keylan118@gmail.com>
2024-03-20 11:52:35 -0600

    Add check for authentication to connect_pod(), remove webID storing

--- pods.ts
+++ pods.ts
@@ -37,11 +37,13 @@ let path = require('path');
 const STORAGE_ENT_NAME : string = "__pods_storageURL";
 const MODULE_NAME : string = "pods";
 const MAX_FILE_SIZE : number = 500;
-const WEB_ID_ENT_NAME : string = "__pods_webID";
 const CLIENT_ID_ENT_NAME : string = "__pods_clientID";
 const CLIENT_SECRET_ENT_NAME : string = "__pods_clientSecret";
 const TOKEN_URL_ENT_NAME : string = "__pods_tokenURL";
 let authFetch : (typeof fetch);
+const ACCESS_TOKEN_ENT_NAME : string = "__pods_accessToken";
+const ACCESS_TOKEN_RECEIVE_TIME_ENT_NAME : string = "__pods_accessTokenReceiveTime"
+const ACCESS_TOKEN_VALID_DURATION_ENT_NAME : string = "__pods_accessTokenValidDuration"
 
 /**
  * Standardizes URLs by checking for slashes at the start and end of URLs, 
@@ -84,25 +86,55 @@ const getClientSecret = krl.Function([], async function() : Promise<string | und
 	return this.rsCtx.getEnt(CLIENT_SECRET_ENT_NAME);
 });
 const setClientSecret = krl.Action(["new_Secret"], async function(new_Secret : string | undefined) {
-	//new_Secret = standardizeURL(new_Secret);
 	this.rsCtx.putEnt(CLIENT_SECRET_ENT_NAME, new_Secret);
 });
-const getWebID = krl.Function([], async function() : Promise<string | undefined> {
-	return this.rsCtx.getEnt(WEB_ID_ENT_NAME);
-});
-const setWebID = krl.Action(["new_ID"], async function(new_ID : string | undefined) {
-	//new_ID = standardizeURL(new_ID);
-	this.rsCtx.putEnt(WEB_ID_ENT_NAME, new_ID);
-});
-const getTokenURL = krl.Function([], async function() : Promise<string | undefined> {
+const getRefreshTokenURL = krl.Function([], async function() : Promise<string | undefined> {
 	return this.rsCtx.getEnt(TOKEN_URL_ENT_NAME);
 });
-const setTokenURL = krl.Action(["new_URL"], async function(new_URL : string | undefined) {
-	//new_URL = standardizeURL(new_URL);
+const setRefreshTokenURL = krl.Action(["new_URL"], async function(new_URL : string | undefined) {
 	this.rsCtx.putEnt(TOKEN_URL_ENT_NAME, new_URL);
 });
+const getAccessToken = krl.Function([], async function() : Promise<string | undefined> {
+	return this.rsCtx.getEnt(ACCESS_TOKEN_ENT_NAME);
+});
+const setAccessToken = krl.Action(["new_Token"], async function(new_Token : string | undefined) {
+	this.rsCtx.putEnt(ACCESS_TOKEN_ENT_NAME, new_Token);
+});
+const getAccessTokenReceiveTime = krl.Function([], async function() : Promise<number | undefined> {
+	return this.rsCtx.getEnt(ACCESS_TOKEN_RECEIVE_TIME_ENT_NAME);
+});
+const setAccessTokenReceiveTime = krl.Action(["new_obj"], async function(new_obj : number | undefined) {
+	this.rsCtx.putEnt(ACCESS_TOKEN_RECEIVE_TIME_ENT_NAME, new_obj);
+});
+const getAccessTokenValidDuration = krl.Function([], async function() : Promise<number | undefined> {
+	return this.rsCtx.getEnt(ACCESS_TOKEN_VALID_DURATION_ENT_NAME);
+});
+const setAccessTokenValidDuration = krl.Action(["new_obj"], async function(new_obj : number | undefined) {
+	this.rsCtx.putEnt(ACCESS_TOKEN_VALID_DURATION_ENT_NAME, new_obj);
+});
 
-
+function getCurrentTimeInSeconds() : number {
+	return new Date().getTime() / 1000;
+}
+const hasValidAccessToken = krl.Function([], async function() {
+	let accessToken = await getAccessToken(this, []);
+	let receiveTime = await getAccessTokenReceiveTime(this, []);
+	let validDuration = await getAccessTokenValidDuration(this, []);
+	if (!accessToken) {
+		return false;
+	}
+	if (accessToken && receiveTime && validDuration) {
+		const currentTime = getCurrentTimeInSeconds();
+		if (currentTime-receiveTime > validDuration) {
+			this.log.debug("Current Time: " + String(currentTime) + ", " +
+							"Receive Time: " + String(receiveTime));
+			this.log.debug("Valid Duration: " + String(validDuration) + ", " +
+							"Current Duration: " + String(currentTime - receiveTime))
+			return false;
+		}
+	}
+	return true;
+});
 const isStorageConnected = krl.Function([], async function() : Promise<boolean> {
 	if (!await getStorage(this, [])) {
 		return false;
@@ -112,16 +144,18 @@ const isStorageConnected = krl.Function([], async function() : Promise<boolean> 
 
 async function createFileObject(fileURL : string) : Promise<File> {
 	let data : Blob;
-	let data_type : string | null | undefined;
+	let data_type : string | null;
+	let filename : string | undefined;
 	if (fileURL.startsWith("file://")) {
-		fileURL = fileURL.slice(8);
+		fileURL = fileURL.slice(7);
 		let response = fs.readFileSync(fileURL);
-		data_type = fileURL.split('.').pop();
+		data_type = <string | null>fileURL.split('.').pop();
 		data = new Blob([response]);
 	} else {
     	let response = await fetch(fileURL);
 		//Get file data type
 		data_type = await response.headers.get('content-type');
+		filename = fileURL.split('/').pop()
 		data = await response.blob();
 	}
     
@@ -130,7 +164,7 @@ async function createFileObject(fileURL : string) : Promise<File> {
     };
 
     //Get file name
-    let filename : string | undefined = fileURL.split('/').pop()
+    
 
     let file = new File([data], <string>filename, metadata);
     return file;
@@ -165,7 +199,7 @@ const authenticate = krl.Action([], async function authenticate() {
     // This URL can be found by looking at the "token_endpoint" field at
     // http://localhost:3000/.well-known/openid-configuration
     // if your server is hosted at http://localhost:3000/.
-    const tokenUrl = await getTokenURL(this, []);
+    const tokenUrl = await getRefreshTokenURL(this, []);
     const response = await fetch(tokenUrl, {
     method: 'POST',
     headers: {
@@ -180,17 +214,23 @@ const authenticate = krl.Action([], async function authenticate() {
     // This is the Access token that will be used to do an authenticated request to the server.
     // The JSON also contains an "expires_in" field in seconds,
     // which you can use to know when you need request a new Access token.
-    const { access_token: accessToken } = await response.json();
+	let response_json = await response.json();
+    const { access_token: accessToken } = response_json;
+	const { expires_in : expiresIn } = response_json;
+	const token_now = getCurrentTimeInSeconds();
+	setAccessToken(this, [accessToken]);
+	setAccessTokenReceiveTime(this, [token_now]);
+	setAccessTokenValidDuration(this, [expiresIn]);
+	this.log.debug("Access Token expires in " + String(expiresIn) + " seconds.");
 
     authFetch = await buildAuthenticatedFetch(accessToken, { dpopKey });
 });
 
 
 
-const connect_storage = krl.Action(["storageURL", "webID", "clientID", "clientSecret", "tokenURL"], 
+const connect_storage = krl.Action(["storageURL", "clientID", "clientSecret", "tokenURL"], 
 									async function(
-										storageURL : string, 
-										webID : string, 
+										storageURL : string,
 										clientID : string, 
 										clientSecret : string, 
 										tokenURL : string) {
@@ -198,10 +238,15 @@ const connect_storage = krl.Action(["storageURL", "webID", "clientID", "clientSe
 		throw MODULE_NAME + ":connect_storage cannot connect an already-connected Pico.";
 	}
 	await setStorage(this, [storageURL]);
-	await setWebID(this, [webID]);
 	await setClientID(this, [clientID]);
 	await setClientSecret(this, [clientSecret]);
-	await setTokenURL(this, [tokenURL]);
+	await setRefreshTokenURL(this, [tokenURL]);
+
+	await authenticate(this, []);
+	if (!await hasValidAccessToken(this, [])) {
+		throw MODULE_NAME + ":authenticate could not authenticate.";
+	}
+
 	return getStorage(this, []);
 });
 const disconnect_storage = krl.Action([], async function() {
@@ -209,14 +254,18 @@ const disconnect_storage = krl.Action([], async function() {
 		throw MODULE_NAME + ":disconnect_storage cannot disconnect an unconnected Pico.";
 	}
 	await setStorage(this, [undefined]);
-	await setWebID(this, [undefined]);
 	await setClientID(this, [undefined]);
 	await setClientSecret(this, [undefined]);
-	await setTokenURL(this, [undefined]);
+	await setRefreshTokenURL(this, [undefined]);
+	await setAccessToken(this, [undefined]);
+	await setAccessTokenReceiveTime(this, [undefined]);
+	await setAccessTokenValidDuration(this, [undefined]);
 	return "Disconnected successfully.";
 });
 
 function checkFileURL(fileURL : string, fileName : string) : string {
+	let newFileURL = standardizeURL(fileURL);
+	fileURL = <string>newFileURL;
 	if (!fileURL.endsWith(fileName)) {
 		fileURL = fileURL + fileName;
 	}
@@ -235,7 +284,8 @@ const store = krl.Action(["fetchFileURL", "storeLocation"], async function(fetch
     }*/
 	
     let file : File = await createFileObject(fetchFileURL);
-	storeLocation = checkFileURL(fetchFileURL, file.name);
+	//storeLocation = checkFileURL(storeLocation, file.name);
+	this.log.debug("Store Location: " + storeLocation);
 
     saveFileInContainer(
         storeLocation,
@@ -246,7 +296,7 @@ const store = krl.Action(["fetchFileURL", "storeLocation"], async function(fetch
         this.log.debug("File uploaded successfully!\n");
     })
     .catch(error => {
-        this.log.error("Error uploading file:", error);
+        this.log.error("Error uploading file: ", error.message);
     });
 });
 const overwrite = krl.Action(["fetchFileURL", "storeLocation"], async function(fetchFileURL : string, storeLocation : string) {
@@ -255,7 +305,8 @@ const overwrite = krl.Action(["fetchFileURL", "storeLocation"], async function(f
 	}
 	
     let file : File = await createFileObject(fetchFileURL);
-	storeLocation = checkFileURL(fetchFileURL, file.name);
+	storeLocation = checkFileURL(storeLocation, file.name);
+	this.log.debug("Store Location: " + storeLocation);
 
     overwriteFile(
         storeLocation,
@@ -266,7 +317,7 @@ const overwrite = krl.Action(["fetchFileURL", "storeLocation"], async function(f
         this.log.debug("File uploaded successfully!\n");
     })
     .catch(error => {
-        this.log.error("Error uploading file:", error);
+        this.log.error("Error uploading file: ", error.message);
     });
 });
 const removeFile = krl.Action(["fileURL"], async function(fileURL : string) {
@@ -306,15 +357,14 @@ const copyFile = krl.Action(["fileURL", "targetURL"],
 });
 
 const pods_fetch = krl.Function(["fileURL"], async function(fileURL : string) {
-    getFile(
+    let file = await getFile(
         fileURL,
         { fetch: authFetch }
     )
-    .then((file) => {
-        this.log.debug(`Fetched a ${getContentType(file)} file from ${getSourceUrl(file)}.`);
-        this.log.debug(`The file is ${isRawData(file) ? "not " : ""}a dataset.`);
-        this.log.debug(file + '\n');
-    })
+    this.log.debug(`Fetched a ${getContentType(file)} file from ${getSourceUrl(file)}.`);
+	this.log.debug(`The file is ${isRawData(file) ? "not " : ""}a dataset.`);
+	this.log.debug(file + '\n');
+	return file;
 });
 
 const listItems = krl.Function(["fileURL"], async function(fileURL : string) {
@@ -468,18 +518,20 @@ const pods: krl.Module = {
 	copyFile: copyFile,
 	fetch: pods_fetch,
 	listItems: listItems,
-  findFile: findFile,
+    findFile: findFile,
 	createFolder: createFolder,
 	removeFolder: removeFolder,
 
 	// getStorage : getStorage, //Private KRL helper function, does not need to be exported
-	// setStorage : setStorage, //Private KRL helper function, does not need to be exported
-	// isPodConnected : isPodConnected //Private KRL helper function, does not need to be exported
+	setStorage : setStorage, //Private KRL helper function, does not need to be exported
+	// isPodConnected : isPodConnected, //Private KRL helper function, does not need to be exported
 	//The following are helper functions that are exported for testing
-	setClientID: setClientID,
-	setClientSecret: setClientSecret,
-	setWebID: setWebID,
-	setTokenURL: setTokenURL,
+	setClientID: setClientID, //Private KRL helper function, does not need to be exported
+	setClientSecret: setClientSecret, //Private KRL helper function, does not need to be exported
+	setRefreshTokenURL: setRefreshTokenURL, //Private KRL helper function, does not need to be exported
+
+	getAccessTokenValidDuration : getAccessTokenValidDuration, //Mostly private KRL helper function, but exported since developer may want to know about the token
+	getAccessTokenReceiveTime : getAccessTokenReceiveTime, //Mostly private KRL helper function, but exported since developer may want to know about the token
 
 	authenticate: authenticate,
 	grantAgentAccess: grantAgentAccess,
